27adad5dbb8e94d2a9edf09476c421da37e423b4
Dante Buhl <dbuhl@ucsc.edu>
2025-02-10 11:27:31 -0800

    Worked on some plots. touched up cdf plot files

--- PreCandidacy/rotation/extraction/Om2B30/cdf_plot.py
+++ PreCandidacy/rotation/extraction/Om2B30/cdf_plot.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import xarray as xr
 
 # this file is meant to work for a simdat.cdf file. 
@@ -34,15 +33,15 @@ def FDY(field,dy):
         dy_field[:,:,i+1,:] = (field[:,:,i+2,:]-field[:,:,i,:])/(2*dy)
     return dy_field
 
-cdf_file = xr.open_dataset('simdat1.cdf')
+cdf_file = xr.open_dataset('simdat4.cdf')
 
-#dX = np.zeros(3)
-#dX[0] = cdf_file.dx # need to know these a priori
-#dX[1] = cdf_file.dy
-#dX[2] = cdf_file.dz
+print(cdf_file.variables)
 
+dX = np.zeros(3)
+dX[0] = cdf_file.dx
+dX[1] = cdf_file.dy
+dX[2] = cdf_file.dz
 
-#these arrays are indexed by [t,z,y,x]?
 ux = cdf_file.ux.as_numpy()
 uy = cdf_file.uy.as_numpy()
 uz = cdf_file.uz.as_numpy()
@@ -51,20 +50,7 @@ x = cdf_file.x.as_numpy()
 y = cdf_file.y.as_numpy()
 z = cdf_file.z.as_numpy()
 
-XX, YY = np.meshgrid(x,y)
-
-#wz = FDX(uy, dX[0]) - FDY(ux, dX[1])
-
-fig, ax = plt.subplots()
-
-pc1 = ax.pcolor(XX[:,:],
-    YY[:,:], ux[0,0,:,:].T,
-    norm=colors.Normalize(vmin=-np.max(np.abs(ux[0,0,:,:])),\
-    vmax=np.max(np.abs(ux[0,0,:,:]))), cmap='seismic')
-
-fig.colorbar(pc1, ax=ax)
-
-plt.show()
+wz = FDX(uy, dX[0]) - FDY(ux, dX[1])
 
 
 
