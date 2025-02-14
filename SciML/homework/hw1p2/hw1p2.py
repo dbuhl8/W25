c96a2254719a5b132f04155e810d694c5312498d
Dante <dbuhl@ucsc.edu>
2025-02-13 23:57:07 -0800

    Finished hw2 for mhd

--- SciML/homework/hw1p2/hw1p2.py
+++ SciML/homework/hw1p2/hw1p2.py
@@ -40,6 +40,26 @@ def fd1(num,dx):
     mat[num-1,num-1] = 2
     return mat/(2*dx)
 
+def bdf3(num,dx):
+    if num < 3:
+        print("Error: bdf3 cannot take in less than three data points")
+        return null
+    else:
+        mat = np.zeros([num,num])
+        bdf3_factor = 11/(6*dx)
+        mat += np.diag(np.ones(num-3)*(-2/11),-3)
+        mat += np.diag(np.ones(num-2)*(9/11),-2)
+        mat += np.diag(np.ones(num-1)*(-18/11),-1)
+        mat += np.diag(np.ones(num))
+        mat *= bdf3_factor
+        # boundary values (will use forward / backward difference)
+        # bdf2 
+        mat[2,0:3] = (3/(2*dx))*np.array([1/3, -4/3, 1])
+        # centered finite difference
+        mat[1,0:3] = (1/(2*dx))*np.array([-1, 0, 1])
+        # euler forward
+        mat[0,0:2] = (1/dx)*np.array([-1, 1])
+        return mat
 
 def fd2(num,dx):
     # centered finite difference for intermediate points
@@ -73,6 +93,16 @@ act_theta[3,2] = -8/3
 act_theta[5,2] = 1
 
 
+npact_theta = np.zeros([10,3])
+npact_theta[1,0] = -10
+npact_theta[2,0] = 10
+npact_theta[1,1] = 28
+npact_theta[2,1] = -1
+npact_theta[6,1] = -1
+npact_theta[3,2] = -8/3
+npact_theta[5,2] = 1
+
+
 num_points = 10001
 
 t = np.linspace(0, 50, num_points, True)
@@ -93,9 +123,9 @@ num_iter = [1, 2, 4, 8, 16]
 iter_size = len(num_iter)
 error = np.zeros([3,iter_size])
 
-print()
-print('-----------------------------------------------')
-print()
+#print()
+#print('-----------------------------------------------')
+#print()
 
 
 for i in range(3):
@@ -113,17 +143,17 @@ for i in range(3):
                 small_indices = torch.abs(theta[:,k]) < tol
                 theta[small_indices,k] = 0
                 theta[big_indices,k] = torch.linalg.lstsq(phi[:,big_indices],dy[:,k])[0]
-        print(num_iter[l])
-        print(theta)
+        #print(num_iter[l])
+        #print(theta)
         error[i,l] = torch.sqrt(torch.sum((act_theta - theta)**2))
-        print()
-        print('-----------------------------------------------')
-        print()
+        #print()
+        #print('-----------------------------------------------')
+        #print()
     # perform finite difference method and compare to learned derivatives
     #fd = torch.from_numpy(fd1(num_points,tstep))
     #thing = torch.matmul(fd,yt) - torch.matmul(phi, theta)
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
 colors = cm.rainbow(np.linspace(0,1,iter_size))
 
 label = ['N = 1', ' N = 2', 'N = 4', 'N = 8', 'N = 16']
@@ -131,17 +161,65 @@ label = ['N = 1', ' N = 2', 'N = 4', 'N = 8', 'N = 16']
 #for i in range(3):
     # plot the error for each solution
     #ax.plot(error[:,i], color=colors[i],label=label[i])
-for i in range(iter_size):
-    ax.plot(noise,error[:,i],color=colors[i],label=label[i])
+#for i in range(iter_size):
+    #ax.plot(noise,error[:,i],color=colors[i],label=label[i])
+
+#ax.set_yscale('log')
+#ax.set_xscale('log') #ax.legend()
+#ax.set_xlabel('Variance of Noise')
+#ax.set_ylabel('MSE error of learned coefficients')
+#ax.set_title('SINDy Error (tol = 1e-4)')
+#ax.legend(title='N Iterations')
+
+
+#fig.tight_layout()
+#plt.show()
+#plt.savefig('noisy_coeff_err.pdf')
+
+
+# test out the effect different derivative schemes have on SINDy
+mfd1 = fd1(num_points,tstep)
+mbdf3 = bdf3(num_points,tstep)
+fd1_dy = np.matmul(mfd1,y)
+bdf3_dy = np.matmul(mbdf3,y)
+# perform SINDy algorithm
+# define feature matrix and feature coefficients
+phi = gen_feature_matrix(y,num_points)
+error = np.zeros([2,iter_size])
+
+for l in range(iter_size):
+    theta = np.linalg.lstsq(phi, fd1_dy)[0]
+    for j in range(num_iter[l]):
+        for k in range(3):
+            big_indices = np.abs(theta[:,k]) >= tol
+            small_indices = np.abs(theta[:,k]) < tol
+            theta[small_indices,k] = 0
+            theta[big_indices,k] = np.linalg.lstsq(phi[:,big_indices],fd1_dy[:,k])[0]
+    error[0,l] = np.sqrt(np.sum((npact_theta - theta)**2, axis=None))
+
+for l in range(iter_size):
+    theta = np.linalg.lstsq(phi, bdf3_dy)[0]
+    for j in range(num_iter[l]):
+        for k in range(3):
+            big_indices = np.abs(theta[:,k]) >= tol
+            small_indices = np.abs(theta[:,k]) < tol
+            theta[small_indices,k] = 0
+            theta[big_indices,k] = np.linalg.lstsq(phi[:,big_indices],bdf3_dy[:,k])[0]
+    error[1,l] = np.sqrt(np.sum((npact_theta - theta)**2, axis=None))
+
+fig, ax = plt.subplots()
+
+ax.plot(num_iter, error[0,:], color='red',
+linestyle='dashed', marker='o', markersize=8, label='FD1')
+ax.plot(num_iter, error[1,:], color='blue',
+linestyle='dashed', marker='o', markersize=8, label='BDF3')
 
 ax.set_yscale('log')
-ax.set_xscale('log') #ax.legend()
-ax.set_xlabel('Variance of Noise')
-ax.set_ylabel('MSE error of learned coefficients')
-ax.set_title('SINDy Error (tol = 1e-4)')
-ax.legend(title='N Iterations')
-
+ax.set_ylabel('MSE Error of learned coefficients')
+ax.legend(title='Numerical Method')
+ax.set_title('Comparing Derivative Schemes for SINDy')
+ax.set_xscale('log')
+ax.set_xlabel('Iterations')
 
 fig.tight_layout()
-plt.savefig('noisy_coeff_err.pdf')
-
+plt.savefig('numerical_coeff_err.pdf')
